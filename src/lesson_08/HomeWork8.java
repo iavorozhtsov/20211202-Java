875992d5a8e0c10806112702f752e5d7535cf7e3
iavorozhtsov <iavorozhtsov@gmail.com>
2022-02-07 21:14:07 +0300

    smth

--- src/lesson_08/HomeWork8.java
+++ src/lesson_08/HomeWork8.java
@@ -3,5 +3,18 @@ package lesson_08;
 public class HomeWork8 {
     public static void main(String[] args) {
         System.out.println("С новым годом!");
+//        String s1 = "Java";
+//        String s2 = new String("Java");
+//        System.out.println(s1 == s2);
+//        System.out.println(" ");
+//        System.out.println(s1.equals(s2));
+//
+//        String s = (2 + 2) + "=value";
+//        System.out.println(s);
+
+        int[][] a = new int[5][5];
+        int[] b[] = new int[5][5];
+        int c[][] = new int[5][5];
+        []int[] d = new int[5][5];
     }
 }
